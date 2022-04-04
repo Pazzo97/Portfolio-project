0532086b4ef26ca9906ee34bb4f0029e88250234
Pazzo97 <pazzomuk@gmail.com>
2022-04-04 21:33:25 +0200

    Work on DOM

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
+
 function show() {
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
 }
+
 function close() {
   mainMenu.style.top = '-100%';
 }
+
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 const navLink = document.querySelectorAll('.navLink');
