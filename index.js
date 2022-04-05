8ed7167f5f53659490999ee689305e9faf074ea5
Kelvin Bush <kelybush@gmail.com>
2022-04-05 19:11:55 +0100

    Set up javascript for the pop-up window

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-use-before-define */
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
@@ -18,3 +17,35 @@ openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 const navLink = document.querySelectorAll('.navLink');
 navLink.forEach((n) => n.addEventListener('click', close));
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    // eslint-disable-next-line no-use-before-define
+    openModal(modal);
+  });
+});
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
