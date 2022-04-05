488ce8af4b529d52aa32a2afebd4d6f51bef1fde
Pazzo97 <pazzomuk@gmail.com>
2022-04-05 20:01:59 +0200

    Added pop window css

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
